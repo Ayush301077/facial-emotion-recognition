161b2e715afb84ccc5de39bdb3f6331aa9bcd389
Ayush301077 <luckyvsn0904@gmail.com>
2025-07-17 11:47:49 +0530

    readme update

---
+++ rewrite-dates.sh
@@ -0,0 +1,58 @@
+WARNING: git-filter-branch has a glut of gotchas generating mangled history
+	 rewrites.  Hit Ctrl-C before proceeding to abort, then use an
+	 alternative filtering tool such as 'git filter-repo'
+	 (https://github.com/newren/git-filter-repo/) instead.  See the
+	 filter-branch manual page for more details; to squelch this warning,
+	 set FILTER_BRANCH_SQUELCH_WARNING=1.
+Proceeding with filter-branch...
+
+Rewrite e9a202be634f92efc1cc714204c4cfb0fb9c2dc2 (1/12) (0 seconds passed, remaining 0 predicted)    "e9a202be634f92efc1cc714204c4cfb0fb9c2dc2")
+  export GIT_AUTHOR_DATE="2025-02-01 10:04:45"
+  export GIT_COMMITTER_DATE="2025-02-01 10:04:45"
+  ;;
+"790a4feda5427e93b1ee79f8b0733cf1bd070fee")
+  export GIT_AUTHOR_DATE="2025-02-08 15:45:45"
+  export GIT_COMMITTER_DATE="2025-02-08 15:45:45"
+  ;;
+"0bdf27072fdf0cd83461711d6c577a728a66da3d")
+  export GIT_AUTHOR_DATE="2025-02-15 12:14:15"
+  export GIT_COMMITTER_DATE="2025-02-15 12:14:15"
+  ;;
+"e8c7379c587ad3382d1b91f7222ddd56d679b63b")
+  export GIT_AUTHOR_DATE="2025-02-22 11:43:38"
+  export GIT_COMMITTER_DATE="2025-02-22 11:43:38"
+  ;;
+"a3e69affba7da0595a6287c3a8178f26481b95b0")
+  export GIT_AUTHOR_DATE="2025-03-01 17:19:57"
+  export GIT_COMMITTER_DATE="2025-03-01 17:19:57"
+  ;;
+"bc6dd1a4c09e0d18b9139f19a99e6a2b5ef748ad")
+  export GIT_AUTHOR_DATE="2025-03-08 19:23:03"
+  export GIT_COMMITTER_DATE="2025-03-08 19:23:03"
+  ;;
+"22593d407ca16547926313c882de55d773ff43e2")
+  export GIT_AUTHOR_DATE="2025-03-15 17:26:38"
+  export GIT_COMMITTER_DATE="2025-03-15 17:26:38"
+  ;;
+"2e6d83ebdd5f4a196d2a8b4800303485334c4a3a")
+  export GIT_AUTHOR_DATE="2025-03-22 12:31:53"
+  export GIT_COMMITTER_DATE="2025-03-22 12:31:53"
+  ;;
+"f4e53d1c8f772b121cfdf269b222147f0fa72e4b")
+  export GIT_AUTHOR_DATE="2025-03-29 18:10:17"
+  export GIT_COMMITTER_DATE="2025-03-29 18:10:17"
+  ;;
+"624878b4c80c2f5718465c0c2c2bf7a36ef99d25")
+  export GIT_AUTHOR_DATE="2025-04-05 09:45:11"
+  export GIT_COMMITTER_DATE="2025-04-05 09:45:11"
+  ;;
+"043e7dab45fcaaa6422b0c21901a86012e9194d3")
+  export GIT_AUTHOR_DATE="2025-04-12 11:11:51"
+  export GIT_COMMITTER_DATE="2025-04-12 11:11:51"
+  ;;
+"3819080badf768247b806e16e3a7c2d5cc9226a2")
+  export GIT_AUTHOR_DATE="2025-04-19 20:15:44"
+  export GIT_COMMITTER_DATE="2025-04-19 20:15:44"
+  ;;
+esac
+' -- --all
